3249f763b78b89c7bc4f90ea3ced9d1b57fccf95
evilAlyosha <taha.marouani.1@gmail.com>
2025-09-06 23:13:10 +0100

    Before adding ascii text

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,39 +1,26 @@
+"use client";
+
+import { useRef } from "react";
 import Link from "next/link";
 import Noise from "@/components/Noise";
 import FuzzyText from "@/components/FuzzyText";
-import ProfileCard from '@/components/ProfileCard'
 
 export default function NotFound() {
+    const containerRef = useRef(null); // ⬅️ same as in Component
+
     return (
         <div className="relative w-screen h-screen bg-black text-white overflow-hidden flex items-center justify-center">
-            {/* Full-screen Noise background */}
-            <Noise
-                patternSize={250}
-                patternAlpha={20}
-                patternRefreshInterval={2}
-            />
+            <Noise patternSize={250} patternAlpha={20} patternRefreshInterval={2} />
 
-
-
-            {/* Foreground content */}
             <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center px-6">
-                {/* Large 404 */}
-                <FuzzyText
-                    baseIntensity={0.8}
-                    hoverIntensity={1.5}
-                    enableHover={true}
-                    fontSize="10rem"
-                    fontWeight={900}
-                    color="#fff"
-                >
+                <FuzzyText fontSize="14rem" fontWeight={900} color="#fff">
                     404
                 </FuzzyText>
 
-
-                <p className="mt-4 text-lg text-gray-300">
-                    Oops! The page you’re looking for doesn’t exist.
+                <p className="mt-2 text-2xl text-gray-300 font-medium">
+                    Oops! The page you’re looking for doesn’t exist. <br />
+                    I'd say practice your shooting down below.
                 </p>
-
 
                 <Link
                     href="/"
@@ -42,7 +29,6 @@ export default function NotFound() {
                     Go back home
                 </Link>
             </div>
-
         </div>
     );
 }
